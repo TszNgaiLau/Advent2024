7a886fbb4de6d06fa42caee9e5cc73190cd9619e
TszNgaiLau <145706756+TszNgaiLau@users.noreply.github.com>
2024-12-04 13:17:23 -0500

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+
+        ArrayList<String> fileData = getFileData("src/input");
+
+        int total = 0;
+        for (int i = 0; i < fileData.size(); i++) {
+            String[] split = fileData.get(i).split(" ");
+            int curr = Integer.parseInt(split[0]);
+            int next = Integer.parseInt(split[1]);
+            boolean increase = false;
+            if (curr < next) {
+                increase = true;
+            }
+            int times = 0;
+            for (int j = 0; j < split.length - 1; j++) {
+                curr = Integer.parseInt(split[j]);
+                next = Integer.parseInt(split[j+1]);
+                if (increase && Math.abs(curr - next) < 3) {
+                    times++;
+                }
+                if (!increase && Math.abs(curr - next) < 3) {
+                    times++;
+                }
+                if (times == split.length - 1) {
+                    total++;
+                }
+            }
+        }
+        System.out.println(total);
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
